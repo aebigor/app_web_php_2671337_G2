3c006fb3d6706f74c81dfb0b822b7984eb340816
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-12 19:39:01 -0500

    03 - Implementación de las vistas

--- controllers/Landing.php
+++ controllers/Landing.php
@@ -2,7 +2,9 @@
     class Landing{
         public function __construct(){}
         public function main(){
+            require_once "views/company/header.view.php";
             require_once "views/company/index.view.php";
+            require_once "views/company/footer.view.php";
         }
     }
 ?>
--- views/company/index.view.php
+++ views/company/index.view.php
@@ -1,14 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Empresa</title>
-    <link rel="stylesheet" href="assets/landing/css/styles.css">
-</head>
-<body>
-    <h1>Bienvenidos a mi Empresa</h1>
-
-    <script src="assets/landing/js/scripts.js"></script>
-</body>
-</html>
+        <!-- Contenido -->
+        <header>
+            <h1>Bienvenidos a mi Empresa</h1>
+        </header>
+        <main>
+            <section>
+                Contenido 1
+            </section>
+            <section>
+                Contenido 2
+            </section>
+        </main>
