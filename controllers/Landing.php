73135116542f05803430f00764d037ab8795ae60
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-12 19:14:27 -0500

    03 - Implementación de las vistas

--- controllers/Landing.php
+++ controllers/Landing.php
@@ -1,9 +1,8 @@
 <?php
     class Landing{
         public function __construct(){}
-
         public function main(){
-            echo "Estoy en la acción main del controlador Landing";
+            require_once "views/company/index.view.php";
         }
     }
 ?>
