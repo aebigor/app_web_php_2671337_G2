99bf493ef88a4a7d5aaa375beb47028c4781694e
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-19 20:03:35 -0500

    04 - Modelo de Negocio (POO)

--- controllers/Roles.php
+++ controllers/Roles.php
@@ -1,15 +1,22 @@
 <?php
     require_once "models/Rol.php";
     class Roles{
-
         public function main(){
             echo "Acción main() del controlador Roles";
         }
         // Registrar usuarios
         public function createRol(){
-            $rol = new Rol;
-            $rol->setRolCode("1");            
-            echo "<br>Código Rol: " . $rol->getRolCode();            
+            // Objeto 1
+            $rol_1 = new Rol;
+            $rol_1->setRolCode("1");
+            echo "<br>Código Rol: " . $rol_1->getRolCode();
+            $rol_1->setRolName("admin");            
+            echo "<br>Nombre Rol: " . $rol_1->getRolName();
+            // Objeto 2 - Constructor
+
+            // Objeto 3 - Constructor
+
+            // Utilice una estructura de repetición para mostrar los tres objetos
         }
     }
 ?>
--- models/Rol.php
+++ models/Rol.php
@@ -6,7 +6,17 @@
         private $rolName;
         // Métodos
         # Sobrecarga de Constructores
-        public function __construct(){}
+        public function __construct(){
+            $a = func_get_args();
+            $i = func_num_args();
+            if (method_exists($this, $f = '__construct' . $i)) {
+                call_user_func_array(array($this, $f), $a);
+            } 
+        }
+        public function __construct2($rolCode, $rolName){
+            $this->rolCode = $rolCode;
+            $this->rolName = $rolName;
+        }
         // Métodos set() y get()        
         # rolCode: set()
         public function setRolCode($rolCode){
@@ -15,6 +25,14 @@
         # rolCode: get()
         public function getRolCode(){
             return $this->rolCode;
+        }        
+        # rolName: set()
+        public function setRolName($rolName){
+            $this->rolName = $rolName;
+        }
+        # rolName: get()
+        public function getRolName(){
+            return $this->rolName;
         }        
         // ****** 2da Parte: Persistencia DB (CRUD) ****** //
         public function rolCreate(){
